c5ff7f32497b4fbd65d46dcee14aaaf97ecdef95
Kory Schneider <kory.schneider@gmail.com>
2017-06-19 13:12:54 -0700

    Print build progress

--- build.py
+++ build.py
@@ -15,6 +15,7 @@ subprocess.run(autoprefix_cmd.split(), stdout=subprocess.PIPE).stdout.decode('ut
 # compile ts
 compile_cmd = 'tsc src/tab.ts --outFile build/tab.js'
 subprocess.run(compile_cmd.split(), stdout=subprocess.PIPE).stdout.decode('utf-8')
+print('✔ Compiled src/tab.ts')
 
 # combine
 html, css, js = '', '', ''
@@ -32,8 +33,10 @@ output += html
 output += '<script>' + js + '</script>'
 output += '<style>' + css + '</style>'
 write(output)
+print('✔ Wrote index.html')
 
 # minify
 minify_command = 'html-minifier --collapse-boolean-attributes --collapse-whitespace --decode-entities --html5 --minify-css --minify-js --process-conditional-comments --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --remove-tag-whitespace --sort-attributes --sort-class-name --trim-custom-fragments --use-short-doctype ' + output_file
 mini_output = subprocess.run(minify_command.split(), stdout=subprocess.PIPE).stdout.decode('utf-8')
 write(mini_output)
+print('✔ Minified index.html')
