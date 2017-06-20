1b007f66443ec25dd450e6ae49de85921beb856f
Kory Schneider <kory.schneider@gmail.com>
2017-06-20 11:32:00 -0700

    Rebuild

--- build/tab.js
+++ build/tab.js
@@ -1,6 +1,8 @@
 window.onload = function () {
 };
-// Load settings
+//
+// Settings
+//
 try {
     var SETTINGS = JSON.parse(localStorage.getItem('settings'));
 }
@@ -8,21 +10,44 @@ catch (e) {
     loadSettings(); // will create defaults if first time on page
 }
 function loadSettings() {
+    //TODO
 }
 function saveSettings() {
+    //TODO
 }
-function handleKeyDown(e) {
-    var keycode;
-    if (window.event) {
-        keycode = window.event.keyCode;
+//
+// Commands
+//
+function default_cmd(url, search, query) {
+    if (search === void 0) { search = ''; }
+    if (query === void 0) { query = ''; }
+    // Components should be URL encoded (or not)
+    // in their respective command functions
+    if (query !== '') {
+        redirect(url + search + query);
     }
-    else if (e) {
-        keycode = e.which;
-    }
-    if (keycode == 13) {
-        interpret();
+    else {
+        redirect(url);
     }
 }
+var google = function (args) {
+    if (args.length > 0) {
+        default_cmd('google.com', '/search?q=', args[0]);
+    }
+    else {
+        default_cmd('google.com');
+    }
+};
+var reddit = function (args) {
+    //TODO
+};
+//
+// Shortcuts
+//
+var shortcuts = {
+    'g': google,
+    'r': reddit
+};
 function interpret() {
     var inputBox = $('#input');
     var input = inputBox.val();
@@ -38,8 +63,37 @@ function interpret() {
             return false;
         }
     }
+    // Parse & format input
+    var args = input.split(';');
+    var command = args[0].trim();
+    args = args.slice(1, args.length);
+    for (var i = 0; i < args.length; i++) {
+        args[i] = args[i].trim();
+    }
+    // Execute
+    var keys = Object.keys(shortcuts);
+    for (var i = 0; i < keys.length; i++) {
+        if (command == keys[i]) {
+            shortcuts[command](args);
+        }
+    }
 }
 function redirect(url) {
+    url = (/(http(s)?:\/\/.)/.test(url))
+        ? url
+        : 'http://' + url;
     window.location.href = url;
     return false;
+}
+function handleKeyDown(e) {
+    var keycode;
+    if (window.event) {
+        keycode = window.event.keyCode;
+    }
+    else if (e) {
+        keycode = e.which;
+    }
+    if (keycode == 13) {
+        interpret();
+    }
 }
